c9d3ba7a02b2c2c365603c434b5985e3352519b9
Martina <martina@palma>
2021-02-23 16:27:37 +0100

    change var in const

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import './App.css';
 
 
 function App() {
-    var today = new Date();
-    var date = today.getFullYear();
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        /header> <
-        main >
-        <
-        h1 > My First React < /h1> <
-        /main> <
-        footer > Copyright by Me { date } < /footer> <
-        /div>
-    );
+  const today = new Date();
+  const date = today.getFullYear();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+        <h1>My First React</h1>
+        </main>
+        <footer>Copyright by Me {date}</footer>
+    </div>
+  );
 }
 
 export default App;
