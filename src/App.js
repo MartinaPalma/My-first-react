042e8a3c66fb48e8a4eb1ea52f074b275cecc089
Martina <martina@palma>
2021-02-24 14:37:27 +0100

    added consola button and build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const consola = require('consola');
 
 function App() {
+  const onClick = () => {
+    consola.info("Ok, hai cliccato qui");
+  }
   const today = new Date();
   const date = today.getFullYear();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={onClick}>Clicca qui</button>
         </header>
         <main>
         <h1>My First React</h1>
